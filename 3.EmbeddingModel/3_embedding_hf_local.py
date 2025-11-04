26245c89a3dfa26188f68957c078175f4506db91
Hemant Kumar <hkumar@ex2india.local>
2025-11-04 10:40:57 +0530

    Completed Embedding models

--- 3.EmbeddingModel/3_embedding_hf_local.py
+++ 3.EmbeddingModel/3_embedding_hf_local.py
@@ -6,4 +6,11 @@ text = "Delhi is the capital of India."
 
 vector = embedding.embed_query(text)
 
+# FOR DOCUMENTS USE THIS
+# documents = ["Delhi is the capital of India.",
+#              "Paris is the capital of France.",
+#              "Kolkata is the capital of West Bengal."]
+
+# reponse = embeddings.embed_documents(documents)
+
 print("Embedding from HuggingFace Local Model", str(vector))
